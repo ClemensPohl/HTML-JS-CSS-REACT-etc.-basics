81b1a997ae427dd9d07916f4f7b9bf48100cc439
ClemensPohl <clemens.pohl.cc@gmail.com>
2022-04-28 08:21:14 +0200

    Orf Site finished, starting js

---
+++ java-script/js-02-schleifen.js
@@ -0,0 +1,31 @@
+// Loops 1
+console.log("schleifen")
+
+for(let count = 0; count < 10; count++){
+    console.log(count + "Legende")
+}
+
+console.log("finished")
+
+// Loops 2
+
+for(let count1 = 7; count1 <=45; count1 ++){
+    console.log(count1)
+}
+
+for(let count2 = 9; count2 <=99; count2 ++){
+    console.log(count2)
+}
+
+for(let count3 = 10; count3 > 0; count3--){
+    console.log(count3)
+}
+
+for(let count4 = 1234; count4 >= 1207; count4--){
+    console.log(count4)
+}
+
+for(let i = 0; i < 8; i++){
+    console.log("Clemens")
+}
+
---
+++ java-script/js-04-random.js
@@ -0,0 +1,4 @@
+let r1 = Math.random() * 100
+let randomAsInteger = Math.round(r1)
+
+console.log(randomAsInteger)
